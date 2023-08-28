051195a14ecebb690ccb2ece933aa76877c99280
Alexander Gladysh <agladysh@gmail.com>
2023-08-28 21:23:51 +0300

    breaking: typo fix in export name (Nearly -> Nearley)

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ function compileNearley(
   return generate(c, opts.export)
 }
 
-export function NearlyPlugin(
+export function NearleyPlugin(
   opts: NearleyPluginOptions = defaultNearleyPluginOptions
 ): Plugin {
   return {
@@ -80,4 +80,4 @@ export function NearlyPlugin(
   }
 }
 
-export default NearlyPlugin(defaultNearleyPluginOptions)
+export default NearleyPlugin(defaultNearleyPluginOptions)
